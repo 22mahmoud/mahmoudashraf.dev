66384daaa697a7a0d60b1dfd04a0613680a360bd
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2026-02-16 00:30:45 +0200

    fix ip

--- bin/django-entrypoint.sh
+++ bin/django-entrypoint.sh
@@ -13,5 +13,6 @@ exec gunicorn config.wsgi \
   --log-level info \
   --max-requests 1000 \
   --log-config-json logconfig.json \
+  --forwarded-allow-ips "*" \
   --no-control-socket \
   --max-requests-jitter 100
